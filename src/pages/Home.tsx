52e810bcc70bdabb64431f430c232d31093f51ef
Rajakakanih <adi.busyrolana@gmail.com>
2026-05-17 10:18:06 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,7 +57,7 @@ export default function Home() {
               transition={{ duration: 0.6, delay: 0.1 }}
               className="text-5xl md:text-6xl font-extrabold text-slate-900 tracking-tight leading-tight mb-6"
             >
-              Selamat Datang Di Potal <br className="hidden md:block" />
+              Selamat Datang Di Portal <br className="hidden md:block" />
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-500">
                 Penerimaan Murid Baru (PMB) MTs Manbaul Ulum Astambul
               </span>
